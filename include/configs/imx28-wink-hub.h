7cc919476e8cdef3e1053590cc24751f9c648aa0
Fabio Estevam <festevam@gmail.com>
2023-09-27 12:09:35 -0300

    imx28-wink-hub: Adjust CONFIG_BOOTCOMMAND
    Adjust CONFIG_BOOTCOMMAND to allow booting an image from NAND.
    
    While at it, remove CFG_EXTRA_ENV_SETTINGS, as it is unused.
    
    Signed-off-by: Fabio Estevam <festevam@gmail.com>

--- include/configs/imx28-wink-hub.h
+++ include/configs/imx28-wink-hub.h
@@ -8,16 +8,6 @@
 #define PHYS_SDRAM_1_SIZE		0x04000000	/* 64 MB of RAM */
 #define CFG_SYS_SDRAM_BASE		PHYS_SDRAM_1
 
-/* UBI and NAND partitioning */
-
-/* Extra Environment */
-#define CFG_EXTRA_ENV_SETTINGS \
-	"image=zImage\0" \
-	"console_fsl=ttyAM0\0" \
-	"console_mainline=ttyAMA0\0" \
-	"fdt_file=imx28-wink-hub-v1.dtb\0" \
-	"fdt_addr=0x41000000\0" \
-
 /* The rest of the configuration is shared */
 #include <configs/mxs.h>
 
